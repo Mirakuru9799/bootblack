9def03514cfa4c735fc9d9fae7d9ece77cdf6101
Mirakuru9799 <66163879+Mirakuru9799@users.noreply.github.com>
2022-06-02 14:35:22 +0000

    Deploying to gh-pages from @ Mirakuru9799/bootblack@049cf0f7b7718c2765375a5cf8d31eb31dcca374 🚀

---
+++ index.0fee9c7c.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".hamburger").click((function(){$(this).toggleClass("is-active")})),$(".hamburger").click((function(){$(".mobile-menu").toggleClass("visible")}))}));
+//# sourceMappingURL=index.0fee9c7c.js.map
---
+++ index.17cf3136.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".hamburger").click((function(){$(this).toggleClass("is-active")})),$(".hamburger").click((function(){$(".mobile-menu").toggleClass("visible")}))}));
+//# sourceMappingURL=index.17cf3136.js.map
