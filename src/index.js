aa3ef4c8a79c14db86f2eccd67b5ca114fc6c9c7
Marina Hrytsai <polanic@ukr.net>
2022-06-03 17:42:43 +0300

    add mobile menu

--- src/index.js
+++ src/index.js
@@ -6,3 +6,15 @@ $(document).ready(function () {
     $('.mobile-menu').toggleClass('visible');
   });
 });
+
+$('.container-mobile div').click(function () {
+  //slide up all the link lists
+  $('.container-mobile ul ul').slideUp();
+  $('.plus', this).html('+');
+  //slide down the link list below the h3 clicked - only if its closed
+  if (!$(this).next().is(':visible')) {
+    $(this).next().slideDown();
+    $('.plus').html('+');
+    $('.plus', this).html('-');
+  }
+});
