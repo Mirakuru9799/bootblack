5b35eaf27d749e294f09c26cd06a5e1b6b686dee
Marina Hrytsai <polanic@ukr.net>
2022-06-06 22:50:49 +0300

    fix bugs

--- src/index.js
+++ src/index.js
@@ -55,3 +55,60 @@ $('a[data-rel^=lightcase]').lightcase();
     carousels();
   })($);
 })();
+
+(function () {
+  'use strict';
+
+  var carousels = function () {
+    $('.owl-carousel1').owlCarousel({
+      loop: true,
+      //autoplay: 1,
+      center: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false,
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false,
+        },
+        1000: {
+          items: 3,
+          nav: true,
+        },
+      },
+    });
+    $('.owl-carousel2').owlCarousel({
+      loop: true,
+      //autoplay: 1,
+      center: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false,
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false,
+        },
+        1000: {
+          items: 3,
+          nav: true,
+        },
+      },
+    });
+  };
+
+  (function ($) {
+    carousels();
+  })($);
+})();
